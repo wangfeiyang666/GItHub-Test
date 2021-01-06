1d2f1909f01dfbfd0e571b17384bc06c6b782468
Lucas0817 <Yodc906>
2021-01-06 10:39:33 +0800

    我是你们爸爸

--- src/main/java/com/yuntu/demo/controller/StudentController.java
+++ src/main/java/com/yuntu/demo/controller/StudentController.java
@@ -2,8 +2,10 @@ package com.yuntu.demo.controller;
 
 import com.yuntu.demo.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/student")
 public class StudentController {
     @Autowired
